edcced7c9b7b128deeb4dc320e4ef1ae8faccaec
Jun <backfto@gmail.com>
2025-05-19 18:05:28 +0900

    임시

---
+++ ios/simplelckwidget/simplelckwidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  simplelckwidgetBundle.swift
+//  simplelckwidget
+//
+//  Created by Jun on 5/18/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct simplelckwidgetBundle: WidgetBundle {
+    var body: some Widget {
+        simplelckwidget()
+        simplelckwidgetControl()
+    }
+}
