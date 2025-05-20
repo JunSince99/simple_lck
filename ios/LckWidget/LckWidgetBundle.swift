61fa90ee60bfd0c0d8f6b993c35a3ad3e5457852
Jun <backfto@gmail.com>
2025-05-20 10:31:39 +0900

    위젯 안되는 버전

---
+++ ios/LckWidget/LckWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  LckWidgetBundle.swift
+//  LckWidget
+//
+//  Created by Jun on 5/19/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct LckWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        LckWidget()
+    }
+}
